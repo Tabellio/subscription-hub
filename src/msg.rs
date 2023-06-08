25e6377546811748b063b47a9fb390f12735bc5a
findolor <anakisci@gmail.com>
2023-06-08 14:05:32 +0300

    Add comments

--- src/msg.rs
+++ src/msg.rs
@@ -10,12 +10,14 @@ pub struct InstantiateMsg {}
 
 #[cw_serde]
 pub enum ExecuteMsg {
+    // Create a new organization
     CreateOrganization {
         name: String,
         description: String,
         website: Option<String>,
         metadata: Option<BTreeMap<String, String>>,
     },
+    // Create a new subscription plan for an organization
     CreateSubscriptionPlan {
         name: String,
         description: String,
@@ -26,9 +28,11 @@ pub enum ExecuteMsg {
         cancelable: bool,
         refundable: bool,
     },
+    // Subscribe to a subscription plan
     SubscribePlan {
         plan_id: u64,
     },
+    // Cancel a subscription plan
     CancelPlan {
         plan_id: u64,
     },
@@ -37,18 +41,25 @@ pub enum ExecuteMsg {
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {
+    // Get the organization with the given ID
     #[returns(Organization)]
     Organization { organization_id: u32 },
+    // Get all organizations owned by the given user
     #[returns(Vec<Organization>)]
     UserOrganizations { user_address: String },
+    // Get the subscription plan with the given ID
     #[returns(SubscriptionPlan)]
     SubscriptionPlan { plan_id: u64 },
+    // Get all subscription plans owned by the given organization
     #[returns(Vec<SubscriptionPlan>)]
     SubscriptionPlans { organization_id: u32 },
+    // Get the subscription with the given ID
     #[returns(Subscription)]
     Subscription { subscription_id: u64 },
+    // Get all subscriptions owned by the given user
     #[returns(Vec<Subscription>)]
     UserSubscriptions { user_address: String },
+    // Get all subscriptions for the given subscription plan
     #[returns(Vec<Subscription>)]
     SubscriptionPlanSubscriptions { plan_id: u64 },
 }

aa60d2425705b3e735285e26f5cbfdb90800f02e
findolor <anakisci@gmail.com>
2023-06-08 11:46:02 +0300

    Add messages

--- src/msg.rs
+++ src/msg.rs
@@ -1,15 +1,54 @@
+use std::collections::BTreeMap;
+
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::{Timestamp, Uint128};
+
+use crate::state::{Organization, Subscription, SubscriptionPlan};
 
 #[cw_serde]
 pub struct InstantiateMsg {}
 
 #[cw_serde]
 pub enum ExecuteMsg {
-    CreateOrganization {},
-    CreateSubscriptionPlan {},
-    SubscribeToPlan {},
+    CreateOrganization {
+        name: String,
+        description: String,
+        website: Option<String>,
+        metadata: Option<Vec<(String, String)>>,
+    },
+    CreateSubscriptionPlan {
+        name: String,
+        description: String,
+        payment_amount: Uint128,
+        expiration: Timestamp,
+        features: Option<Vec<String>>,
+        metadata: Option<BTreeMap<String, String>>,
+        cancelable: bool,
+        refundable: bool,
+    },
+    SubscribePlan {
+        plan_id: u64,
+    },
+    CancelPlan {
+        plan_id: u64,
+    },
 }
 
 #[cw_serde]
 #[derive(QueryResponses)]
-pub enum QueryMsg {}
+pub enum QueryMsg {
+    #[returns(Organization)]
+    Organization { organization_id: u32 },
+    #[returns(Vec<Organization>)]
+    UserOrganizations { user_address: String },
+    #[returns(SubscriptionPlan)]
+    SubscriptionPlan { plan_id: u64 },
+    #[returns(Vec<SubscriptionPlan>)]
+    SubscriptionPlans { organization_id: u32 },
+    #[returns(Subscription)]
+    Subscription { subscription_id: u64 },
+    #[returns(Vec<Subscription>)]
+    UserSubscriptions { user_address: String },
+    #[returns(Vec<Subscription>)]
+    SubscriptionPlanSubscriptions { plan_id: u64 },
+}

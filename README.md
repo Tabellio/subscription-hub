c1806733ea5b5e86de4e078ac5ed1c9a622f7cd0
findolor <anakisci@gmail.com>
2023-06-08 11:44:35 +0300

    Update state

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ CosmWasm Subscription Hub is a decentralized application (dApp) that allows proj
 - **Cancel and Refund Flows**: Provide users with the ability to cancel their subscriptions and initiate refund processes, adhering to predefined cancellation policies and refund rules.
 - **Multi-Organization Support**: Allow projects to create and manage multiple organizations, each with its own set of subscription plans. Users can subscribe to plans offered by different organizations.
 - **Metadata and Customization**: Add metadata and custom data to the subscription plans and organizations, providing additional information and flexibility for project owners and users.
+- **Native and CW20 Tokens**: Support for native tokens and CW20 tokens, allowing projects to accept payments in different currencies.
 
 ## License
 
@@ -20,4 +21,3 @@ This project is licensed under the [Apache License, Version 2.0](./LICENSE). Fee
 For any questions or inquiries about the CosmWasm Subscription Hub, please reach out to the project maintainers:
 
 - Maintainer Name: [findolor](https://github.com/findolor)
-
--- src/state.rs
+++ src/state.rs
@@ -1,7 +1,8 @@
+use std::collections::BTreeMap;
+
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Addr, Timestamp, Uint128};
 use cw_storage_plus::{Item, Map};
-use std::collections::BTreeMap;
 
 #[cw_serde]
 pub struct Organization {
@@ -27,6 +28,14 @@ const ORGANIZATIONS: Map<u32, Organization> = Map::new("organizations");
 const USER_ORGANIZATIONS: Map<Addr, Vec<u32>> = Map::new("user_organizations");
 
 #[cw_serde]
+pub enum DurationUnit {
+    Day,
+    Week,
+    Month,
+    Year,
+}
+
+#[cw_serde]
 pub struct SubscriptionPlan {
     // The address of the organization that owns the subscription plan
     pub organization_id: u32,
@@ -36,8 +45,10 @@ pub struct SubscriptionPlan {
     pub description: String,
     // Price of the subscription plan
     pub payment_amount: Uint128,
-    // Expiration time of the subscription plan
-    pub expiration: Timestamp,
+    // Duration of the subscription plan
+    pub duration: u8,
+    // Unit of the duration of the subscription plan
+    pub duration_unit: DurationUnit,
     // List of features of the subscription plan
     pub features: Option<Vec<String>>,
     // Key-Value metadata of the subscription plan
@@ -57,3 +68,28 @@ const SUBSCRIPTION_PLANS: Map<u64, SubscriptionPlan> = Map::new("subscription_pl
 // The organization_subscription_plans map stores the organization_id -> subscription_plan_id
 const ORGANIZATION_SUBSCRIPTION_PLANS: Map<u32, Vec<u64>> =
     Map::new("organization_subscription_plans");
+
+#[cw_serde]
+pub struct Subscription {
+    // The address of the subscriber
+    pub subscriber: Addr,
+    // Subscription plan id
+    pub plan_id: u64,
+    // Subscription expiration
+    pub expiration: Timestamp,
+    // Whether the subscription is canceled
+    pub canceled: bool,
+}
+
+// The subscription_id is the primary key for the subscription
+const SUBSCRIPTION_ID: Item<u64> = Item::new("subscription_id");
+
+// The subscriptions map stores the subscription_id -> subscription
+const SUBSCRIPTIONS: Map<u64, Subscription> = Map::new("subscriptions");
+
+// The subscriber_subscriptions map stores the subscriber -> subscription_id
+const SUBSCRIBER_SUBSCRIPTIONS: Map<Addr, Vec<u64>> = Map::new("subscriber_subscriptions");
+
+// The subscription_plan_subscriptions map stores the subscription_plan_id and subscriber -> subscription_id
+const SUBSCRIPTION_PLAN_SUBSCRIPTIONS: Map<(u64, Addr), u64> =
+    Map::new("subscription_plan_subscriptions");
